d267475c90919b0f82f08084fe72f0f42304ca2b
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2019-12-28 20:58:34 +0700

    assignment1

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        public int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+                return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 f1 = new Class1();
+            Console.WriteLine("Nhap n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 1; ; i++)
+            {
+                if (n > f1.Fibonacci(i) & n < f1.Fibonacci(i + 1))
+                {
+                    Console.WriteLine(f1.Fibonacci(i));
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
