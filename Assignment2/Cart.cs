e27fed6edc3c1b1fb665efe347670c565bccdfe9
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-01-05 21:52:01 +0700

    Assignment2

---
+++ Assignment2/Cart.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Cart
+    {
+        private int Id;
+        private string customer;
+        private int grandTotal;
+        private string city;
+        private string country;
+        public List<Product> productlist = new List<Product>();
+
+        public int id
+        {
+            get
+            {
+                return Id;
+            }
+
+            set
+            {
+                Id = value;
+            }
+        }
+
+        public string Customer
+        {
+            get
+            {
+                return customer;
+            }
+
+            set
+            {
+                customer = value;
+            }
+        }
+
+        public int GrandTotal
+        {
+            get
+            {
+                return grandTotal;
+            }
+
+            set
+            {
+                grandTotal = value;
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+
+            set
+            {
+                city = value;
+            }
+        }
+
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+
+            set
+            {
+                country = value;
+            }
+        }
+    }
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action a1 = new Action();
+            a1.AddCustomer();
+
+            string Chon;
+            while (true)
+            {
+                Console.WriteLine("Chon 1 de them san pham.");
+                Console.WriteLine("Chon 2 de xoa sp.");
+                Console.WriteLine("Chon 3 de hien thi toan bo thong tin.");
+                Console.WriteLine("Chon 4 de Tinh tong tien.");
+
+                Chon = Console.ReadLine();
+                switch (Chon)
+                {
+                    case "1":
+                        a1.addproduct();
+                        break;
+                    case "2":
+                        a1.RemoveProduct();
+
+                        break;
+                    case "3":
+                        a1.ShowCustomer();
+                        a1.ShowProduct();
+                        break;
+                    case "4":
+                        a1.TotalMoney();
+                        break;
+                }
+            }
+                Console.ReadKey();
+        }
+    }
+}
