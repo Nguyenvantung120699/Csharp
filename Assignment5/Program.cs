221db2f734f92faafdd3c4c81c59c29ac6d4be76
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-01-13 14:02:51 +0700

    Add files via upload

---
+++ Assignment5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.InputCustomer();
+        }
+    }
+}
